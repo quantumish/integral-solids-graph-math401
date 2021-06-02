3337b230387c3203f19e88598fec8b81270766c7
David Freifeld <freifeld.david@gmail.com>
2021-06-02 14:13:04 -0700

    Add window sizing, minor tweaks

--- threed.py
+++ threed.py
@@ -9,7 +9,7 @@ from typing import Final, List, Tuple
 
 SPACIOUS: Final[float] = 1.1
 
-fig: plt.Figure = plt.figure()
+fig: plt.Figure = plt.figure(figsize=(8, 5))
 ax: Axes = fig.add_subplot(1, 1, 1, projection='3d')
 plt.subplots_adjust(bottom=0.2)
 
@@ -29,7 +29,7 @@ def draw(f: str, bound: float, shift: float) -> None:
     x_smooth: np.ndarray = np.arange(0,bound*SPACIOUS,0.1)
     y_smooth: List[float] = [eval(f) for x in x_smooth]
     y_max: float = max(y_smooth)
-    y_min: float  = min(y_smooth)
+    y_min: float = min(y_smooth)
     ax.plot(x_smooth, y_smooth, label="f(x)")
     ax.plot([bound,bound], [y_min*SPACIOUS if (y_min < 0) else 0, y_max*SPACIOUS], label="bound")
     ax.axes.set_xlim3d(left=0, right=bound*SPACIOUS)
@@ -39,12 +39,14 @@ def draw(f: str, bound: float, shift: float) -> None:
     plt.draw()
     
 f_axbox: Axes = plt.axes([0.1, 0.05, 0.8, 0.075])
-b_axbox: Axes = plt.axes([0.1, 0.15, 0.1, 0.075])
-s_axbox: Axes = plt.axes([0.25, 0.15, 0.1, 0.075])
 f_text_box: TextBox = TextBox(f_axbox, 'f(x)', initial="math.sqrt(x)")
 f_text_box.on_submit(lambda text: draw(text, float(cache[1]), float(cache[2])))
+
+b_axbox: Axes = plt.axes([0.1, 0.15, 0.1, 0.075])
 b_text_box: TextBox = TextBox(b_axbox, 'bound ', initial="9")
 b_text_box.on_submit(lambda text: draw(cache[0], float(text), float(cache[2])))
+
+s_axbox: Axes = plt.axes([0.25, 0.15, 0.1, 0.075])
 s_text_box: TextBox = TextBox(s_axbox, 'shift ', initial="0.3")
 s_text_box.on_submit(lambda text: draw(cache[0], float(cache[1]), float(text)))
 
