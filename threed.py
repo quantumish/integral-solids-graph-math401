192a821917c833162e1aec3edefc305421d6d52c
David Freifeld <freifeld.david@gmail.com>
2021-06-01 23:32:44 -0700

    Flexibility, added a bug

--- threed.py
+++ threed.py
@@ -1,33 +1,47 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib.widgets import TextBox
 import math
 import numpy as np
 
-X_BOUND = 10
-Y_BOUND = 4
-Z_BOUND = 5
+SPACIOUS = 1
 
 shift = 0.3
 
 fig = plt.figure()
-ax = Axes3D(fig)
 
-i = 0
-while (i < 9):
-  verts = [[(i,math.sqrt(i)/2,math.sqrt(i)/2), (i,0,0), (i,math.sqrt(i),0)]]
-  coll = Poly3DCollection(verts)
-  coll.set_edgecolor("#000000")
-  ax.add_collection3d(coll)
-  i+=shift
+ax = fig.add_subplot(1, 1, 1, projection='3d')
+plt.subplots_adjust(bottom=0.2)
 
-x = np.arange(0,X_BOUND,0.1)
-ax.plot(x, np.sqrt(x), label="y=sqrt(x)")
-ax.plot([9,9], [0,Y_BOUND], label="x=9")
+def draw(f="math.sqrt(x)", bound=9):
+    ax.clear()
+    x = 0
+    while (x < 9):
+        verts = [[(x,eval(f)/2,eval(f)/2), (x,0,0), (x,eval(f),0)]]
+        coll = Poly3DCollection(verts)
+        coll.set_edgecolor("#000000")
+        ax.add_collection3d(coll)
+        x+=shift
+    x_smooth = np.arange(0,bound*SPACIOUS,0.1)
+    y_smooth = [eval(f) for x in x_smooth]
+    y_max = max(y_smooth)
+    y_min = min(y_smooth)
+    ax.plot(x_smooth, y_smooth, label="y=%s"%f)
+    ax.plot([bound,bound], [y_min,y_max], label="x=%d"%bound)
+    ax.axes.set_xlim3d(left=0, right=10)
+    print(y_min, y_max)
+    ax.axes.set_ylim3d(bottom=y_min*SPACIOUS, top=y_max*SPACIOUS)
+    ax.axes.set_zlim3d(bottom=(y_min/2), top=(y_max/2))
+    plt.draw()
 
-ax.axes.set_xlim3d(left=0, right=X_BOUND)
-ax.axes.set_ylim3d(bottom=0, top=Y_BOUND)
-ax.axes.set_zlim3d(bottom=0, top=Z_BOUND)
-plt.legend()
+def f_submit(func):
+    draw(func)
+
+
+axbox = plt.axes([0.1, 0.05, 0.8, 0.075])
+f_text_box = TextBox(axbox, 'f(x)', initial="math.sqrt(x)")
+f_text_box.on_submit(f_submit)
+draw()
 plt.show()
 
