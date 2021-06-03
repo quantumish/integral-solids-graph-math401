ba7843fe22676c93f9b643a21c456813f14ee64b
zbuster05 <zakary.saya@gmail.com>
2021-06-02 17:47:50 -0700

    Moved text boxes bc that was my job :P

--- threed.py
+++ threed.py
@@ -11,7 +11,8 @@ SPACIOUS: Final[float] = 1.1
 
 fig: plt.Figure = plt.figure(figsize=(8, 5))
 ax: Axes = fig.add_subplot(1, 1, 1, projection='3d')
-plt.subplots_adjust(bottom=0.2)
+plt.subplots_adjust(bottom=0.25)
+plt.subplots_adjust(top=1)
 
 cache: List[str] = ["math.sqrt(x)", "9", "0.3"]
 
@@ -38,16 +39,16 @@ def draw(f: str, bound: float, shift: float) -> None:
     ax.legend()
     plt.draw()
     
-f_axbox: Axes = plt.axes([0.1, 0.05, 0.8, 0.075])
-f_text_box: TextBox = TextBox(f_axbox, 'f(x)', initial="math.sqrt(x)")
+axbox: Axes = plt.axes([0.1, 0.03, 0.8, 0.075])
+f_text_box: TextBox = TextBox(axbox, 'f(x)', initial="math.sqrt(x)")
 f_text_box.on_submit(lambda text: draw(text, float(cache[1]), float(cache[2])))
 
-b_axbox: Axes = plt.axes([0.1, 0.15, 0.1, 0.075])
+b_axbox: Axes = plt.axes([0.1, 0.13, 0.35, 0.075])
 b_text_box: TextBox = TextBox(b_axbox, 'bound ', initial="9")
 b_text_box.on_submit(lambda text: draw(cache[0], float(text), float(cache[2])))
 
-s_axbox: Axes = plt.axes([0.25, 0.15, 0.1, 0.075])
-s_text_box: TextBox = TextBox(s_axbox, 'shift ', initial="0.3")
+sxbox: Axes = plt.axes([0.55, 0.13, 0.35, 0.075])
+s_text_box: TextBox = TextBox(sxbox, 'step', initial="0.3")
 s_text_box.on_submit(lambda text: draw(cache[0], float(cache[1]), float(text)))
 
 draw(cache[0], float(cache[1]), float(cache[2]))
